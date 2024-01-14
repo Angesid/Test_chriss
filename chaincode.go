2a2c6ca05c517b3885d5e6851cfe3b1ce0fe62a9
Angesid <angel.sideris@ssl-unipi.gr>
2024-01-14 23:22:04 +0200

    minor

--- chaincode.go
+++ chaincode.go
@@ -9,10 +9,10 @@ import (
 func main() {
 	itemChaincode, err := contractapi.NewChaincode(&chaincode.SmartContract{})
 	if err != nil {
-		log.Panicf("Error creating asset-transfer-basic chaincode: %v", err)
+		log.Panicf("Error creating Parsing chaincode: %v", err)
 	}
 
 	if err := itemChaincode.Start(); err != nil {
-		log.Panicf("Error starting asset-transfer-basic chaincode: %v", err)
+		log.Panicf("Error starting Parsing chaincode: %v", err)
 	}
 }
